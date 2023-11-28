8ea1a8c2be745a86be492f76591b2aae65ebbfd7
rahuljoshi1729 <rahul22100@akgec.ac.in>
2023-11-28 17:29:07 +0530

    added(api):for rest link

--- ERP_Django/ERP/login/views.py
+++ ERP_Django/ERP/login/views.py
@@ -434,7 +434,7 @@ class PasswordReset(APIView):
             def patch(self, request):
                 token=request.headers.get('token')
                 print(request.data)
-                token = request.data.get('data', {}).get('token')
+                token = request.data.get('token')
                 print(token)
                 if token is None:
                     return JsonResponse({'error': 'token is required','status':400}, status=400)      # Handle the case where 'email' is not provided
