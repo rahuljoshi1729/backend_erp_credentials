a9e699d71e75837ca653a4426570744d1d0bc136
rahuljoshi1729 <rahul22100@akgec.ac.in>
2023-11-30 20:26:37 +0530

    clear

--- ERP_Django/ERP/ERP/settings.py
+++ ERP_Django/ERP/ERP/settings.py
@@ -73,6 +73,18 @@ CORS_ALLOWED_METHODS=[
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 
 MIDDLEWARE = [
